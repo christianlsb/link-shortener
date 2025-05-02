1ee54e6da7089bab6953ab3ebc31a72d26489edc
christianlsb <chrislsb09@gmail.com>
2025-05-02 12:36:34 -0300

    Adiciona suporte a CORS e ajusta rotas para encurtamento de links

--- backend/main.py
+++ backend/main.py
@@ -5,7 +5,9 @@ import pymongo
 import hashlib
 from hashids import Hashids
 from fastapi import status
- 
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import RedirectResponse
+
 myclient = pymongo.MongoClient("mongodb://localhost:27017/")
 mydb = myclient["mydatabase"]
 mycol = mydb["links"]
@@ -13,6 +15,18 @@ mycol = mydb["links"]
 hashids = Hashids(salt="lsb", min_length=6)
 
 app = FastAPI()
+
+origins = [
+   "*",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 class Link(BaseModel):
     long_link: str
@@ -27,13 +41,14 @@ async def create_shorter_link(link: Link):
     hashed = convert_str_for_hashid_md5(link)
     mydict = { "keyLink": hashed, "originalLink": link.model_dump() }
     x = mycol.insert_one(mydict)
-    return hashed
+    return 'http://127.0.0.1:8000/' + hashed
 
-@app.get("/api/shorter/{keyLink}")
+@app.get("/{keyLink}")
 def get_original_link(keyLink: str):
     result = mycol.find_one({"keyLink": keyLink}, {"_id": 0, "originalLink": 1})
-
+    print(result)
     if result:
-        return result 
+        return RedirectResponse(url=result.get("originalLink").get("long_link"))
     else:
         return {"error": "Link not found"}
+    
